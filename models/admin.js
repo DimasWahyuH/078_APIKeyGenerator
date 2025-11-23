ccff7f8887226f3c356ac1c95b1c3508a8ee9ebc
dimas <dimaswh19@gmail.com>
2025-11-23 22:56:19 +0700

    Menambahkan model Admin dengan atribut email dan password

---
+++ models/admin.js
@@ -0,0 +1,11 @@
+// models/admin.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Admin = sequelize.define('Admin', {
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false } // Akan disimpan dalam bentuk hash
+});
+
+
+module.exports = Admin;
